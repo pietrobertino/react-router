ccbd3244a9dd53c504b8c685d1d7d31afd1cf6f5
pietrobertino <pietrobertino20@gmail.com>
2026-03-20 13:15:22 +0100

    style active navbar item

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -8,7 +8,7 @@ export default function AppHeader() {
             <div className="container">
                 <nav className="d-flex justify-content-center my-3">
                     {menu.map(navItem => (
-                        <NavLink key={navItem.id} to={navItem.path} className=" text-decoration-none px-3 text-black" id="nav-item">{navItem.text}</NavLink>
+                        <NavLink key={navItem.id} to={navItem.path} className=" text-decoration-none px-3" id="nav-item">{navItem.text}</NavLink>
                     ))}
                 </nav>
             </div>
